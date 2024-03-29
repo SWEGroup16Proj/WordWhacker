0def6b218ac5d1d32a2c26db5634d1ec90e7eb9c
Jmorehe1 <112976229+Jmorehe1@users.noreply.github.com>
2024-03-29 01:00:17 -0500

    Leaderboard Update
    Sorting now works. Saving the code to a json works. Loading is having issues due to line 40, will try to debug. First line of leaderboard keeps defaulting to 000 AAA

--- WordWhacker/Assets/HighscoreTable.cs
+++ WordWhacker/Assets/HighscoreTable.cs
@@ -24,21 +24,58 @@ public class HighscoreTable : MonoBehaviour
        
         // Deactivate the entry template object
         entryTemplate.gameObject.SetActive(false);
+       /* Tester
         highscoreEntryList=new List<HighscoreEntry>(){
             new HighscoreEntry{score = 777, name ="AAA"},
-            new HighscoreEntry{score = 690, name ="CAT"},
+            new HighscoreEntry{score = 425, name ="CAT"},
             new HighscoreEntry{score = 680, name ="MAR"},
             new HighscoreEntry{score = 420, name ="GLO"},
+            new HighscoreEntry{score = 800, name ="LOL"},
         };
+        */
 
+        string jsonString=PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores=JsonUtility.FromJson<Highscores>(jsonString);
+
+          if (highscores == null) {
+            // There's no stored table, initialize
+            Debug.Log("Initializing table with default values...");
+            AddHighscoreEntry(999, "CMK");
+            AddHighscoreEntry(555, "JOE");
+            AddHighscoreEntry(333, "DAV");
+            AddHighscoreEntry(222, "CAT");
+            AddHighscoreEntry(111, "MAX");
+            AddHighscoreEntry(444, "AAA");
+            // Reload
+            jsonString = PlayerPrefs.GetString("highscoreTable");
+            highscores = JsonUtility.FromJson<Highscores>(jsonString);
+        }
+        //Sorter
+        for(int i =0;i<highscoreEntryList.Count;i++){
+            for(int j=i+1;j<highscoreEntryList.Count;j++){
+                if(highscoreEntryList[j].score>highscoreEntryList[i].score)
+                {
+                    //swap
+                    HighscoreEntry tmp=highscoreEntryList[i];
+                    highscoreEntryList[i]=highscoreEntryList[j];
+                    highscoreEntryList[j]=tmp;
+                }
+            }
+        }
         highscoreEntryTransformList=new List<Transform>();
         foreach (HighscoreEntry highscoreEntry in highscoreEntryList){
             CreateHighscoreEntryTransform(highscoreEntry,entryContainer,highscoreEntryTransformList);
         }
-        {
-            
-        }
        
+       //saving system
+       /*
+       Testers
+       Highscores highscores=new Highscores{highscoreEntryList=highscoreEntryList};
+       string json=JsonUtility.ToJson(highscores);
+       PlayerPrefs.SetString("highscoreTable",json);
+       PlayerPrefs.Save();
+       Debug.Log(PlayerPrefs.GetString("highscoreTable"));
+       */
     }
 //Function to create/add entries into the list
 
@@ -47,7 +84,7 @@ private void CreateHighscoreEntryTransform(HighscoreEntry highscoreEntry, Transf
 
         // Loop to instantiate entry templates for testing purposes
             // Instantiate entry template as a child of the entry container
-            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
+            Transform entryTransform = Instantiate(entryTemplate, container);
 
             // Get RectTransform component of the instantiated entry template
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
@@ -70,7 +107,28 @@ private void CreateHighscoreEntryTransform(HighscoreEntry highscoreEntry, Transf
 
         transformList.Add(entryTransform);
 }
+
+private class Highscores{
+    public List<HighscoreEntry> highscoreEntryList;
+}
+
+private void AddHighscoreEntry(int score, string name) {
+        // Create HighscoreEntry
+        HighscoreEntry highscoreEntry = new HighscoreEntry { score = score, name = name };
+        
+        // Load saved Highscores
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+        if (highscores == null) {
+            // There's no stored table, initialize
+            highscores = new Highscores() {
+                highscoreEntryList = new List<HighscoreEntry>()
+            };
+        }
+}
 //Represents single high score entry
+[System.Serializable]
     private class HighscoreEntry{
         public int score; 
         public string name;
